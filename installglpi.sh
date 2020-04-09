76fc5bd0aa42a9dbad17742a6e2207df8dbe525f
Thomas Clauzel <t.clauzel@gmail.com>
2020-04-09 21:28:55 +0200

    Update installglpi.sh

--- installglpi.sh
+++ installglpi.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
+# title : installglpi.sh
+# author : tclauzel
+# description : this is a script to install GLPI easly (automated install of GLPI)
+# the script install and configure apache and PHP and download the last GLPI
+# HOW TO USE : 
+# chmod +x install.sh 
+# ./install.sh
+echo "#####################################"
+echo "Define configuration"
+echo "#####################################"
+mysqlpwd='root'
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+# ---------------------------------------------
+#
+# VERIFY RUN AS ROOT
+#
+# ---------------------------------------------
+if [[ $EUID -ne 0 ]]; then
+    echo "Work like a true hacker ! run the script as ROOT !"
+    exit 1
+fi
 echo "#####################################"
 echo "installing dependencies"
 echo "#####################################"
 apt-get install apache2 php libapache2-mod-php -y
-apt-get install php-imap php-ldap php-curl php-xmlrpc php-gd php-mysql php-cas -y
+apt-get install php-imap php-ldap php-curl php-xmlrpc php-gd php-mysql php-cas php-xml php-mbstring -y
 apt-get install mariadb-server -y
 apt-get install apcupsd php-apcu -y
-apt-get install php-mbstring -y
-apt-get install php-xml -y
+echo "#####################################"
+echo "Apache2 Configuration"
+echo "#####################################"
 echo "
 <Directory /var/www/html/glpi>
         Options Indexes FollowSymLinks
@@ -15,16 +41,13 @@ echo "
         Require all granted
 </Directory>
 " >> /etc/apache2/apache2.conf
-#
-# DO NOT WORK
-#
-#apt-get install phpmyadmin -y
-echo "#####################################"
+echo '<meta http-equiv=refresh" content="0;URL=./glpi">' > /var/www/html/index.html
+echo "#####################################"	
 echo "securising mariadb-server"
 echo "#####################################"
-# you can try to add this : https://gist.github.com/Mins/4602864 
+echo "setup password 'root' for the root user"
 apt install expect -y
-MYSQL_ROOT_PASSWORD=root
+MYSQL_ROOT_PASSWORD=$mysqlpwd
 SECURE_MYSQL=$(expect -c "
 
 set timeout 10
@@ -57,34 +80,34 @@ echo "restarting servies"
 echo "#####################################"
 /etc/init.d/apache2 restart
 /etc/init.d/mysql restart
-# create database 
+echo "#####################################"
+echo "Database configuration"
+echo "#####################################"
 mysql -uroot <<MYSQL_SCRIPT
 create database glpidb;
 grant all privileges on glpidb.* to glpiuser@localhost identified by "glpipassword";
 quit
 MYSQL_SCRIPT
 echo "#####################################"
-
-echo "database name : glpidb "
-
-echo "database user : glpiuser and password : glpipassword"
-
+echo "database server : localhost"
+echo "database name : glpidb"
+echo "database user : glpiuser"
+echo "database password : glpipassword"
 echo "#####################################"
-
+echo ""
+echo ""
 echo "#####################################"
-echo "setup web server"
+echo "Installing GLPI"
 echo "#####################################"
-# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+glpiv=$(get_latest_release "glpi-project/glpi")
+echo "downloading last detect version : $glpiv"
 cd /usr/src/
-wget https://github.com/glpi-project/glpi/releases/download/9.4.5/glpi-9.4.5.tgz
-tar -xvzf glpi-9.4.5.tgz -C /var/www/html
+wget https://github.com/glpi-project/glpi/releases/download/$glpiv/glpi-$glpiv.tgz
+tar -xvzf glpi-$glpiv.tgz -C /var/www/html
 chown -R www-data:www-data /var/www/html/glpi/
-echo '<meta http-equiv=refresh" content="0;URL=./glpi">' > /var/www/html/index.html
-echo "plase no open http://127.0.0.1/glpi/ in you borwser and finish the installation"
-echo "
-     glpi/glpi pour le compte administrateur
-    tech/tech pour le compte technicien
-    normal/normal pour le compte normal
-    post-only/postonly pour le compte postonly
+echo "Open http://127.0.0.1/glpi/ or http://myserverip/glpi/ in you borwser and finish the installation"
+#    glpi/glpi pour le compte administrateur
+#    tech/tech pour le compte technicien
+#    normal/normal pour le compte normal
+#    post-only/postonly pour le compte postonly
 
-"
